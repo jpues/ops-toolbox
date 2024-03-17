9b84e915a7efe28e1179c2238e5359c00cb37e06
jpues <67596569+jpues@users.noreply.github.com>
2024-03-17 14:56:52 -0400

    Update install-ansible.sh

--- files/install-ansible.sh
+++ files/install-ansible.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # install ansible using python
 python_version="python3.11"
-if ! command -v $python_version 2>&1 >/dev/null; then
+if ! command -v $python_version &>/dev/null; then
     sudo dnf -y install epel-release
     sudo dnf -y install $python_version $python_version-pip $python_version-setuptools
     sudo $python_version -m pip install --upgrade pip
-    if ! command -v ansible 2>&1 >/dev/null; then
+    if ! command -v ansible &>/dev/null; then
         $python_version -m pip install ansible
     fi
 fi
